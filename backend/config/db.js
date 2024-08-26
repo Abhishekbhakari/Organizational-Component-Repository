2f79e4c736e4784a38dfbbc6811e14e4fc271c83
AbhishekBhakari <bhakariabhi@gmail.com>
2024-08-26 19:12:02 +0530

    fixed database connection string for deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,17 @@
+require('dotenv').config();
+
 const mongoose = require('mongoose');
 const config = require('config');
-const db = config.get('mongoURI');
+const db = process.env.MONGO_URI;
 
+console.log(db)
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+    
     console.log('MongoDB connected...');
   } catch (err) {
     console.error(err.message);
